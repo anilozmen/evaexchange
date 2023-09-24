85c921e495a14e0fc4267c6ad413a274c2cc2613
Anıl Özmen <me@anilozmen.com>
2023-09-24 19:50:33 +0300

    sequelize config connected to .env file

--- src/config/config.js
+++ src/config/config.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
+
 module.exports = {
 	development: {
-		username: 'root',
-		password: 'password',
-		database: 'evatech',
-		host: '127.0.0.1',
+		username: process.env.DB_USER_DEV,
+		password: process.env.DB_PASSWORD_DEV,
+		database: process.env.DB_NAME_DEV,
+		host: process.env.DB_HOST_DEV,
 		dialect: 'postgres'
 	  },
 	  test: {
-		username: 'root',
-		password: null,
-		database: 'database_test',
-		host: '127.0.0.1',
+		username: process.env.DB_USER_TEST,
+		password: process.env.DB_PASSWORD_TEST,
+		database: process.env.DB_NAME_TEST,
+		host: process.env.DB_HOST_TEST,
 		dialect: 'postgres'
 	  },
 	  production: {
-		username: 'root',
-		password: null,
-		database: 'database_production',
-		host: '127.0.0.1',
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
 		dialect: 'postgres'
 	  }
 }
