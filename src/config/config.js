51e1b369bd21595128a3cdd1901d736abb2f31e6
Anıl Özmen <me@anilozmen.com>
2023-09-25 14:56:44 +0300

    docker config changes

--- src/config/config.js
+++ src/config/config.js
@@ -17,9 +17,9 @@ module.exports = {
 	  },
 	  production: {
 		username: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
+		password: process.env.DB_PW,
 		database: process.env.DB_NAME,
-		host: process.env.DB_HOST,
+		host: 'postgres',
 		dialect: 'postgres'
 	  }
 }
